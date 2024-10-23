d396615f69f87a381dc637ae57741f7ecb390a87
Bovis ALOUKOU <bovisaloukou@gmail.com>
2024-10-23 07:39:23 +0100

    Ajout de la fonctionnalité de vérification d'email

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-// /config/db.js
 const mongoose = require('mongoose');
 require('dotenv').config();  // Charger les variables d'environnement
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://etudiant:etudiant@cluster0.eutao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
     try {
-        const dbURI = MONGO_URI || 'mongodb+srv://etudiant:etudiant@cluster0.eutao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-        await mongoose.connect(dbURI);
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Connexion à MongoDB réussie');
     } catch (error) {
         console.error('Erreur de connexion à MongoDB :', error);
-        process.exit(1);
+        process.exit(1);  // Arrêter l'application en cas d'échec
     }
 };
 
