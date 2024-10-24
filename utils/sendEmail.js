b8227aca4a18b2b855c1019b6caba2b4cfb6370b
Bovis ALOUKOU <bovisaloukou@gmail.com>
2024-10-24 03:36:27 +0100

    vérification d'email toujours en utilisant maintenant sendinblue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,29 @@
-const mailgun = require('mailgun-js');
+const nodemailer = require('nodemailer');
 
-// Configuration Mailgun
-const mg = mailgun({ 
-    apiKey: process.env.MAILGUN_API_KEY,  // Utilisez votre clé API privée Mailgun ici
-    domain: process.env.MAILGUN_DOMAIN    // Assurez-vous que le domaine est correct (sandbox ou domaine réel)
+// Créez un transporteur Nodemailer avec Sendinblue
+const transporter = nodemailer.createTransport({
+  service: 'SendinBlue',
+  auth: {
+    user: process.env.SENDINBLUE_EMAIL,  // Votre email Sendinblue
+    pass: process.env.SENDINBLUE_API_KEY // Votre clé API Sendinblue
+  }
 });
 
 const sendEmail = async (to, subject, text) => {
-    try {
-        const mailOptions = {
-            from: `postmaster@${process.env.MAILGUN_DOMAIN}`,  // Utilisez une adresse valide correspondant au domaine
-            to,                                               // Destinataire
-            subject,                                          // Sujet de l'email
-            text                                              // Contenu de l'email
-        };
+  try {
+    const mailOptions = {
+      from: 'eleonorelysiane@gmail.com',  // L'adresse email d'envoi
+      to,                               // Destinataire
+      subject,                          // Sujet de l'email
+      text                              // Contenu de l'email
+    };
 
-        // Envoi de l'email avec Mailgun
-        const result = await mg.messages().send(mailOptions);
-        console.log('Email envoyé :', result);
-    } catch (error) {
-        console.error('Erreur lors de l\'envoi de l\'email :', error);
-        throw new Error('Erreur lors de l\'envoi de l\'email');
-    }
+    const result = await transporter.sendMail(mailOptions);
+    console.log('Email envoyé :', result);
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi de l\'email', error);
+    throw new Error('Erreur lors de l\'envoi de l\'email');
+  }
 };
 
 module.exports = sendEmail;
