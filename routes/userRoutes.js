4c9c9886250212e46e228cf99db3bc560d8d5ab3
Bovis ALOUKOU <bovisaloukou@gmail.com>
2024-10-26 03:09:36 +0100

    Changer le post qui est utilisé pour la route /verify-email en get dans le backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,9 @@ const userController = require('../controllers/userController');
 // Routes utilisateurs
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.post('/verify-email', userController.verifyEmail);  // Nouvelle route pour vérifier l'email
 
 // Route pour récupérer tous les utilisateurs
 router.get('/users', userController.getAllUsers);  // Ajoutez l'authentification si nécessaire
+router.get('/verify-email', userController.verifyEmail);  // Nouvelle route pour vérifier l'email
 
 module.exports = router;
